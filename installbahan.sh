5ac2621adb623e60a5a103790eec72e58fdecf15
thharandjah <64585193+thharandjah@users.noreply.github.com>
2020-05-01 01:14:21 +0700

    Add files via upload

---
+++ installbahan.sh
@@ -0,0 +1,58 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+echo $green"Proses install agak lama cuy, tergantung jaringan Lu...."
+sleep 3
+echo
+echo $white "INSTALL NANO"
+pkg install nano
+echo $red "INSTALL FIGLET"
+pkg install figlet
+sleep 1
+echo $green "INSTALL PYTHON2"
+pkg install python2
+sleep 1
+echo $blue "INSTALL COWSAY"
+pkg install cowsay
+sleep 1
+echo $yellow "INSTALL RUBY"
+pkg install ruby
+gem install lolcat
+sleep 1
+echo $purple "INSTALL TOILET"
+pkg install toilet
+sleep 1
+echo $blue "INSTALL PHP"
+pkg install php
+sleep 1
+echo $red "INSTALL CODING BUAT HACK" 
+pkg install bash
+pkg install requests
+pkg install mechanize
+sleep 1
+echo $yellow "INSTALL MODULE HACK"
+pip2 install requests
+pip2 install mechanize
+sleep 1
+echo $green "PROSES HAMPIR SELESAI..." 
+apt install jq -y
+sleep 1
+echo $white "TUNGGU BEBERAPA DETIK" 
+sleep 1
+figlet 3
+sleep 1
+figlet 2
+sleep 1
+figlet 1
+sleep 1
+echo $blue "SUCCESSFUL..."
+echo $green "PROSES SELESAI, ANDA SUDAH BISA MENGGUNAKAN SCRIPT INI" 
+sleep 1
+clear
